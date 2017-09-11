50abc0bf05c67b99ea15faacf7e8240b5b4893f2
SekouD <sekou.diao@gmail.com>
2017-09-11 18:22:21 +0200

    bumpversion test 4

--- lyricsmaster/__init__.py
+++ lyricsmaster/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """SekouD"""
 __email__ = 'sekoud.python@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='lyricsmaster',
-    version='0.1.3',
+    version='0.1.4',
     description="LyricsMaster is a library for analyzing lyrics using various Machine Learning algorithms. It includes utilities for downloading lyrics from multiple lyrics providers.",
     long_description=readme + '\n\n' + history,
     author="SekouD",
