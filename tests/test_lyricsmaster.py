604a7afe49bca832861730dfdbeb60bc78e6d437
SekouD <sekou.diao@gmail.com>
2017-09-19 00:11:09 +0200

    Added before_install script to install and configure tor.

--- tests/test_lyricsmaster.py
+++ tests/test_lyricsmaster.py
@@ -9,6 +9,10 @@ from click.testing import CliRunner
 
 from bs4 import BeautifulSoup, Tag
 
+import requests
+from stem import Signal
+from stem.control import Controller
+
 from lyricsmaster import lyricsmaster
 from lyricsmaster import cli
 from lyricsmaster import lyricsprovider
@@ -156,3 +160,35 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+def test_tor():
+    def renew_connection():
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate(password="password")
+            controller.signal(Signal.NEWNYM)
+
+    def get_tor_session():
+        session = requests.session()
+        # Tor uses the 9050 port as the default socks port
+        session.proxies = {'http': 'socks5://127.0.0.1:9050',
+                           'https': 'socks5://127.0.0.1:9050'}
+        return session
+
+    # Make a request through the Tor connection
+    # IP visible through Tor
+    session = get_tor_session()
+    print(session.get("http://httpbin.org/ip").text)
+    # Above should print an IP different than your public IP
+
+    # Following prints your normal public IP
+    print(requests.get("http://httpbin.org/ip").text)
+
+    renew_connection()
+    # Make a request through the Tor connection
+    # IP visible through Tor
+    session = get_tor_session()
+    print(session.get("http://httpbin.org/ip").text)
+    # Above should print an IP different than your public IP
+
+    # Following prints your normal public IP
+    print(requests.get("http://httpbin.org/ip").text)
