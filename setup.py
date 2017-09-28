ceebc1c51a22bac5f0b67c4a0345ba366ea2e397
SekouD <sekou.diao@gmail.com>
2017-09-28 10:22:34 +0200

    bumped version to v2.7.2 after tweaking the tests.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ test_requirements = [
 
 setup(
     name='lyricsmaster',
-    version='2.7.1',
+    version='2.7.2',
     description="LyricsMaster is a library for downloading lyrics from multiple lyrics providers. LyricWiki, AzLyrics, Genius and other lyrics provider are available",
     long_description=readme + '\n\n' + history,
     author="SekouD",
