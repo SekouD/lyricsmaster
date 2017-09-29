b9592048d224d2dd89acb07d1a0baf5e450d09d5
SekouD <sekou.diao@gmail.com>
2017-09-29 16:19:55 +0200

    bumped version to v2.7.5 after refactoring the code, the documentation and the tests.

--- lyricsmaster/__init__.py
+++ lyricsmaster/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """SekouD"""
 __email__ = 'sekoud.python@gmail.com'
-__version__ = '2.7.4'
+__version__ = '2.7.5'
 
 from .providers import LyricWiki, AzLyrics, Genius
 from .utils import TorController
