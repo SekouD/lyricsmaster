cbf373cc9a7daf84a0304e778b9b144824569ab8
SekouD <sekou.diao@gmail.com>
2017-09-29 16:47:12 +0200

    refactoring of code, tests and documentation

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('USAGE.rst') as usage_file:
-    usage = usage_file.read()
-
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
+
+with open('docs/usage.rst') as usage_file:
+    usage = usage_file.read()
 
 requirements = [
     'Click>=6.0',
