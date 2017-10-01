73a12f0e1ff1229ee0e771b29af1ad6f7fc0f208
SekouD <sekou.diao@gmail.com>
2017-10-01 22:54:06 +0200

    Added MusixMatch to the list of supported Providers.

--- lyricsmaster/providers.py
+++ lyricsmaster/providers.py
@@ -710,10 +710,10 @@ class Genius(LyricsProvider):
 
 class Lyrics007(LyricsProvider):
     """
-        Class interfacing with https://www.lyrics007.com .
-        This class is used to retrieve lyrics from Lyrics007.
+    Class interfacing with https://www.lyrics007.com .
+    This class is used to retrieve lyrics from Lyrics007.
 
-        """
+    """
     base_url = 'https://www.lyrics007.com'
     search_url = base_url + '/search.php?category=artist&q='
     name = 'Lyrics007'
@@ -859,6 +859,137 @@ class Lyrics007(LyricsProvider):
         """
         return text
 
+
+class MusixMatch(LyricsProvider):
+    """
+    Class interfacing with https://www.musixmatch.com .
+    This class is used to retrieve lyrics from MusixMatch.
+
+    """
+    base_url = 'https://www.musixmatch.com'
+    search_url = base_url + '/search/{0}/artists'
+    name = 'MusixMatch'
+
+    def _has_lyrics(self, page):
+        if page.find("p", {'class': 'mxm-lyrics__content '}):
+            return True
+        else:
+            return False
+
+    def _has_artist(self, page):
+        if page.find("div", {'class': 'artist-page main-wrapper'}):
+            return True
+        else:
+            return False
+
+    def _make_artist_url(self, artist):
+        return self.base_url + '/artist/' + artist
+
+    def get_albums(self, raw_artist_page):
+        """
+        Fetches the albums section in the supplied html page.
+
+        :param raw_artist_page: Artist's raw html page.
+        :return: list.
+            List of BeautifulSoup objects.
+        """
+        artist_page = BeautifulSoup(raw_artist_page, 'lxml')
+        albums_link = artist_page.find("li", {'id': 'albums'})
+        albums_link = albums_link.find('a').attrs['href']
+        albums_page = BeautifulSoup(self.get_page(self.base_url + albums_link).data, 'lxml')
+        albums = [tag for tag in albums_page.find_all("div", {'class': 'media-card-text'})]
+        return albums
+
+    def get_album_infos(self, tag):
+        """
+        Extracts the Album title from the tag
+
+        :param tag: BeautifulSoup object.
+        :return: string.
+            Album title.
+        """
+        album_title = tag.find('h2').text
+        release_date = tag.find('h3').text
+        return album_title, release_date
+
+    def get_songs(self, album):
+        """
+        Fetches the links to the songs of the supplied album.
+
+        :param album: BeautifulSoup object.
+        :return: List of BeautifulSoup Link objects.
+        """
+        album_page = BeautifulSoup(self.get_page(self.base_url + album.find('a').attrs['href']).data, 'lxml')
+        album_div = album_page.find("div", {'class': 'mxm-album__tracks mxm-collection-container'})
+        song_links = album_div.find_all("li", {'class': re.compile("^mui-collection__item")})
+        song_links = [song.find('a') for song in song_links]
+        return song_links
+
+    def create_song(self, link, artist, album_title):
+        """
+        Creates a Song object.
+
+        :param link: BeautifulSoup Link object.
+        :param artist: string.
+        :param album_title: string.
+        :return: models.Song object or None.
+        """
+        if not link.attrs['href'].startswith(self.base_url):
+            song_url = self.base_url + link.attrs['href']
+        else:
+            song_url = link.attrs['href']
+        song_title = link.text
+        raw_lyrics_page = self.get_lyrics_page(song_url)
+        if not raw_lyrics_page:
+            return None
+        lyrics_page = BeautifulSoup(raw_lyrics_page, 'lxml')
+        lyrics = self.extract_lyrics(lyrics_page)
+        writers = self.extract_writers(lyrics_page)
+        song = Song(song_title, album_title, artist, lyrics, writers)
+        return song
+
+    def extract_lyrics(self, lyrics_page):
+        """
+        Extracts the lyrics from the lyrics page of the supplied song.
+
+        :param lyrics_page: BeautifulSoup Object.
+            BeautifulSoup lyrics page.
+        :return: string.
+            Formatted lyrics.
+        """
+        lyric_box = lyrics_page.find("p", {'class': re.compile("^mxm-lyrics__content")})
+        lyrics = '\n'.join(lyric_box.strings)
+        return lyrics
+
+    def extract_writers(self, lyrics_page):
+        """
+        Extracts the writers from the lyrics page of the supplied song.
+
+        :param lyrics_page: BeautifulSoup Object.
+            BeautifulSoup lyrics page.
+        :return: string.
+            Song writers or None.
+        """
+        writers_box = lyrics_page.find("p", {'class': re.compile("^mxm-lyrics__copyright")})
+        if writers_box:
+            writers = writers_box.text.strip()
+        else:
+            writers = None
+        return writers
+
+    def _clean_string(self, text):
+        """
+        Cleans the supplied string and formats it to use in a url.
+
+        :param text: string.
+            Text to be cleaned.
+        :return: string.
+            Cleaned text.
+        """
+        text = text.replace(' ', '-').replace('.', '-')
+        if text[-1] == '-':
+            text = text[:-1]
+        return text
 
 if __name__ == "__main__":
     pass
