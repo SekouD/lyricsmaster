8ddfd14fd33f79271367d4a1f01c2eaaf70ae413
SekouD <sekou.diao@gmail.com>
2018-06-09 15:17:06 +0200

    Added more informative comments and reformated the code.

--- lyricsmaster/utils.py
+++ lyricsmaster/utils.py
@@ -12,6 +12,7 @@ import certifi
 import gevent.monkey
 import socket
 
+# Python 2.7 compatibility
 # Works for Python 2 and 3
 try:
     from importlib import reload
@@ -21,8 +22,10 @@ except ImportError:
     except:
         pass
 
+# Python 2.7 compatibility
+# Works for Python 2 and 3
 try:
-    basestring # Python 2.7 compatibility
+    basestring
 except NameError:
     basestring = str
 
@@ -58,13 +61,15 @@ def set_save_folder(folder):
         folder = os.path.join(folder, 'LyricsMaster')
     return folder
 
+
 class TorController:
     """
     Controller class for Tor client.
 
     Allows the Api to make requests over the Tor network.
-    If 'controlport' is None, the library will use the default timing to create a new Tor circuit.
+    If 'controlport' is None, the library will use the default timing to renew the Tor circuit.
     If 'controlport' is passed as an argument, the library will create a new Tor circuit for each new album downloaded.
+    See https://www.torproject.org/docs/tor-manual.html.en for more information on how Tor works.
 
     :param ip: string.
         The IP adress of the Tor proxy.
@@ -75,6 +80,7 @@ class TorController:
     :param password: string.
         The password or control_auth_cookie to authenticate on the Tor CONTROLPORT.
     """
+
     def __init__(self, ip='127.0.0.1', socksport=9050, controlport=None, password=''):
         self.ip = ip
         self.socksport = socksport
@@ -82,39 +88,50 @@ class TorController:
         self.password = password
 
     def __repr__(self):
-        return '{0}.{1}({2}, {3}, {4}, {5})'.format(__name__, self.__class__.__name__, self.ip, self.socksport, self.controlport, self.password)
+        return '{0}.{1}({2}, {3}, {4}, {5})'.format(__name__, self.__class__.__name__, self.ip, self.socksport,
+                                                    self.controlport, self.password)
 
     def get_tor_session(self):
         """
-        Configures the session to use a Tor proxy.
+        Configures and create the session to use a Tor Socks proxy.
 
         :return: urllib3.SOCKSProxyManager object.
         """
-        user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+        user_agent = {'user-agent':
+                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
         session = SOCKSProxyManager('socks5://{0}:{1}'.format(self.ip, self.socksport), cert_reqs='CERT_REQUIRED',
                                     ca_certs=certifi.where(), headers=user_agent)
         return session
 
     def renew_tor_circuit(self):
         """
+        Renews the Tor circuit.
         Sends a NEWNYM message to the Tor network to create a new circuit.
 
         :return: bool.
             Whether a new tor ciruit was created.
 
         """
+
         def renew_circuit(password):
+            """
+            Sends a NEWNYM message to the Tor network to create a new circuit.
+
+            :param password:
+            :return: bool.
+            """
             controller.authenticate(password=password)
             if controller.is_newnym_available():  # true if tor would currently accept a NEWNYM signal.
                 controller.signal(Signal.NEWNYM)
                 print('New Tor circuit created')
-                result= True
+                result = True
             else:
                 delay = controller.get_newnym_wait()
                 print('Delay to create new Tor circuit: {0}s'.format(delay))
-                result= False
+                result = False
             return result
 
+        # Needs to reload the default socket to be able to send the is_newnym_avilable and get_newnym_wait signals
         reload(socket)
         if isinstance(self.controlport, int):
             with Controller.from_port(port=self.controlport) as controller:
@@ -126,5 +143,3 @@ class TorController:
             is_renewed = False
         gevent.monkey.patch_socket()
         return is_renewed
-
-
